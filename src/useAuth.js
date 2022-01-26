3fbf971a8eccdb8ffc8fa18627e77e3d834b39ee
Pramit De <pramitde2011@gmail.com>
2022-01-25 20:39:45 -0600

    updated error display for useAuth

--- src/useAuth.js
+++ src/useAuth.js
@@ -25,6 +25,7 @@ export default function useAuth(code) {
                 }).catch((err) => {
                     window.location = '/'
                     console.log(err)
+                    console.log("please man")
 
                 })
     },      [code])
